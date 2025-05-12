2ab2e8d5dd21b6c04581e2e87bf39edfc6f399a5
lucioacri <lucioacri@gmail.com>
2025-05-12 16:59:10 +0200

    add module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const posts = require("./posts.js");
 
 app.listen(port, () => {
   console.log("Server collgato alla port:" + port);
@@ -13,37 +14,5 @@ app.get("/", (req, res) => {
 app.use(express.static("public"));
 
 app.get("/bacheca", (req, res) => {
-  const postsArray = [
-    {
-      title: "Ciambellone",
-      content: "",
-      image: "/images/ciambellone.jpeg",
-      tags: ["ciambellone"],
-    },
-    {
-      title: "Cracker barbabietola",
-      content: "",
-      image: "/images/cracker_barbabietola.jpeg",
-      tags: ["cracker", "barbabietola"],
-    },
-    {
-      title: "Pane fritto dolce",
-      content: "",
-      image: "/images/pane_fritto_dolce.jpeg",
-      tags: ["pane", "frittura", "dolce"],
-    },
-    {
-      title: "Pasta barbabietola",
-      content: "",
-      image: "/images/pasta_barbabietola.jpeg",
-      tags: ["pasta", "barbabietola"],
-    },
-    {
-      title: "Torta paesana",
-      content: "",
-      image: "/images/torta_paesana.jpeg",
-      tags: ["torta", "paesana"],
-    },
-  ];
-  res.json({ posts: postsArray });
+  res.json({ posts });
 });
---
+++ posts.js
@@ -0,0 +1,34 @@
+const postsArray = [
+  {
+    title: "Ciambellone",
+    content: "",
+    image: "/images/ciambellone.jpeg",
+    tags: ["ciambellone"],
+  },
+  {
+    title: "Cracker barbabietola",
+    content: "",
+    image: "/images/cracker_barbabietola.jpeg",
+    tags: ["cracker", "barbabietola"],
+  },
+  {
+    title: "Pane fritto dolce",
+    content: "",
+    image: "/images/pane_fritto_dolce.jpeg",
+    tags: ["pane", "frittura", "dolce"],
+  },
+  {
+    title: "Pasta barbabietola",
+    content: "",
+    image: "/images/pasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola"],
+  },
+  {
+    title: "Torta paesana",
+    content: "",
+    image: "/images/torta_paesana.jpeg",
+    tags: ["torta", "paesana"],
+  },
+];
+
+module.exports = postsArray;
