80a6607b36f3afd5922f26e873c9ebae0b99ad55
lucioacri <lucioacri@gmail.com>
2025-05-12 14:34:08 +0200

    add express.static and images

--- app.js
+++ app.js
@@ -10,13 +10,40 @@ app.get("/", (req, res) => {
   res.json("Server del mio blog");
 });
 
+app.use(express.static("public"));
+
 app.get("/bacheca", (req, res) => {
   const bacheca = [
-    { title: "Ciambellone" },
-    { title: "Cracker barbabietola" },
-    { title: "Pane fritto dolce" },
-    { title: "Pasta barbabietola" },
-    { title: "Torta paesana" },
+    {
+      title: "Ciambellone",
+      content: "",
+      image: "/images/ciambellone.jpeg",
+      tags: ["", ""],
+    },
+    {
+      title: "Cracker barbabietola",
+      content: "",
+      image: "/images/cracker_barbabietola.jpeg",
+      tags: ["", ""],
+    },
+    {
+      title: "Pane fritto dolce",
+      content: "",
+      image: "/images/pane_fritto_dolce.jpeg",
+      tags: ["", ""],
+    },
+    {
+      title: "Pasta barbabietola",
+      content: "",
+      image: "/images/pasta_barbabietola.jpeg",
+      tags: ["", ""],
+    },
+    {
+      title: "Torta paesana",
+      content: "",
+      image: "/images/torta_paesana.jpeg",
+      tags: ["", ""],
+    },
   ];
   res.json(bacheca);
 });
