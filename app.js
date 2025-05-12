bcedd47aba5ef4c357a4e33bddbe63661f0f865d
lucioacri <lucioacri@gmail.com>
2025-05-12 13:41:24 +0200

    add res and bacheca

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Server collgato alla port:" + port);
+});
+
+app.get("/", (req, res) => {
+  res.json("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  const bacheca = [
+    { title: "Ciambellone" },
+    { title: "Cracker barbabietola" },
+    { title: "Pane fritto dolce" },
+    { title: "Pasta barbabietola" },
+    { title: "Torta paesana" },
+  ];
+  res.json(bacheca);
+});
