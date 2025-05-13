bddbad8fb9dc1f2f3c9a526124c0687175ed4f21
lucioacri <lucioacri@gmail.com>
2025-05-13 13:32:50 +0200

    removed posts from app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const posts = require("./data.js");
 const routerPosts = require("./routers/posts");
 
 app.listen(port, () => {
